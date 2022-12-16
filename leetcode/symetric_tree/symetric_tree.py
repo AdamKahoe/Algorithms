1640f774e7a6d201826e40a0ed08ae2d2d4f159a
Adam Kahoe <89557570+AdamKahoe@users.noreply.github.com>
2022-12-16 00:30:17 -0800

    Add files via upload

---
+++ leetcode/symetric_tree/symetric_tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        queue = [root]
+        while queue:
+            length = len(queue)
+            level = []
+            while length:
+                node = queue.pop(0)
+                level.append(node.val if node else None)
+                length -= 1
+                if node:
+                    queue.append(node.left)
+                    queue.append(node.right)
+            i = 0
+            j = len(level) - 1
+            while i <= j:
+                if level[i] != level[j]:
+                    return False
+                i += 1
+                j -= 1
+        return True
