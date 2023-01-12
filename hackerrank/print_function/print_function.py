8e272d5d0e532b82c4897237d65fe8fa4f8e37a2
Adam Kahoe <89557570+AdamKahoe@users.noreply.github.com>
2023-01-11 21:07:12 -0800

    Add files via upload

--- hackerrank/print_function/print_function.py
+++ hackerrank/print_function/print_function.py
@@ -1,2 +1,6 @@
-for i in range(1, int(input()) + 1):
-    print(i, end='')
+if __name__ == '__main__':
+    n = int(input())
+    full_str = ""
+    for i in range(1, n+1):
+        full_str += str(i)
+    print(full_str)
