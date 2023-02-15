ce4fc9c5f771105ebe96b5443ad26914eaf55d46
Adam Kahoe <89557570+AdamKahoe@users.noreply.github.com>
2023-02-14 21:30:46 -0800

    Add files via upload

--- hackerrank/nested_lists/nested_lists.py
+++ hackerrank/nested_lists/nested_lists.py
@@ -1,11 +1,16 @@
-def secondLowestGrade(classList):
-    secondLowestScore = sorted(set(_[1] for _ in classList))[1]
-    result = sorted([_[0] for _ in classList if _[1] == secondLowestScore])
-    return result
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
 
-
-numberOfStudents = int(input())
-classList = []
-for i in range(numberOfStudents):
-    classList.append([str(input()), float(input())])
-print('\n'.join(secondLowestGrade(classList)))
+    students = sorted(students, key = lambda x: x[1])
+    #print(students)
+    #second_lowest_score = students[1][1]
+    second_lowest_score = sorted(list(set([x[1] for x in students])))[1]
+    desired_students = []
+    for stu in students:
+        if stu[1] == second_lowest_score:
+            desired_students.append(stu[0])
+    print("\n".join(sorted(desired_students)))
